a15d8d031bdff7f549fcb1e9cdbe3379f68e2b5a
dev-techguy <dev.techguy@gmail.com>
2022-05-07 18:26:26 +0300

    :beers: Writing code drunkenly.

--- database/migrations/2022_05_07_151330_create_groups_table.php
+++ database/migrations/2022_05_07_151330_create_groups_table.php
@@ -13,14 +13,8 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('groups', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->foreignId('group_id')->constrained('groups'); // add this line
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -32,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('groups');
     }
 };
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Events\PresenceEvent;
+use App\Events\PrivateEvent;
+use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Route;
+use PHPUnit\TextUI\XmlConfiguration\Group;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +19,48 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/color', function () {
+    return view('color-picker');
+});
+
+Route::post('/fireEvent', function (Request $request) {
+
+    PublicEvent::dispatch($request->color);
+})->name('fire.public.event');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+
+
+Route::middleware('auth')->group(function () {
+    Route::view('/dashboard', 'dashboard')->name('dashboard');
+
+    Route::get(
+        '/private/fireEvent',
+        function () {
+            // faking file upload
+            sleep(3);
+            PrivateEvent::dispatch('Profile picutre has been updated');
+        }
+    )->name('fire.private.event');
+
+
+    Route::get('/dashboard', function () {
+        $groups = Group::where('id', auth()->user()->group_id)->get();
+        return view('dashboard', compact('groups'));
+    }
+    )->name('dashboard');
+
+    Route::get('/dashboard/{group}', function (Request $request, Group $group) {
+
+        abort_unless($request->user()->canJoinGroup($group->id), 401);
+        return view('group', compact('group'));
+    }
+    )->name('group');
+
+    Route::get('/presence/fireEvent/{message}', fn() => PresenceEvent::dispatch())->name('fire.presence.event');
 });
